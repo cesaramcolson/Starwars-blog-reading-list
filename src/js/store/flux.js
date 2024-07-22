044233331ddd7a0e382c79d3515347fe82b0df2b
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-07-22 03:17:56 +0000

    Prevent the website from fetching the Startwars API again if refreshed using local storage to save the store in the browser

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,127 +1,155 @@
 const getState = ({ getStore, getActions, setStore }) => {
     return {
         store: {
-            people: [],
-            peopleInfo: {},
-            planets: [],
-            planetsInfo: {},
-            vehicles: [],
-            vehiclesInfo: {},
-            favorites: []
+            people: JSON.parse(localStorage.getItem("people")) || [],
+            peopleInfo: JSON.parse(localStorage.getItem("peopleInfo")) || {},
+            planets: JSON.parse(localStorage.getItem("planets")) || [],
+            planetsInfo: JSON.parse(localStorage.getItem("planetsInfo")) || {},
+            vehicles: JSON.parse(localStorage.getItem("vehicles")) || [],
+            vehiclesInfo: JSON.parse(localStorage.getItem("vehiclesInfo")) || {},
+            favorites: JSON.parse(localStorage.getItem("favorites")) || []
         },
         actions: {
             getPeople: async () => {
-                try {
-                    const resp = await fetch("https://www.swapi.tech/api/people?page=1&limit=10", {
-                        method: "GET",
-                        headers: {
-                            "Content-type": "application/json"
+                const store = getStore();
+                if (store.people.length === 0) {
+                    try {
+                        const resp = await fetch("https://www.swapi.tech/api/people?page=1&limit=82", {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
                         }
-                    });
-                    if (!resp.ok) {
-                        throw new Error(`error status: ${resp.status}`);
+                        let data = await resp.json();
+                        setStore({ people: data.results });
+                        localStorage.setItem("people", JSON.stringify(data.results));
+                    } catch (error) {
+                        console.error("Error");
                     }
-                    let data = await resp.json();
-                    setStore({ people: data.results });
-                } catch (error) {
-                    console.error("Error");
                 }
             },
             getPeopleInfo: async (id) => {
-                try {
-                    const resp = await fetch(`https://www.swapi.tech/api/people/${id}`, {
-                        method: "GET",
-                        headers: {
-                            "Content-type": "application/json"
+                const store = getStore();
+                if (!store.peopleInfo[id]) {
+                    try {
+                        const resp = await fetch(`https://www.swapi.tech/api/people/${id}`, {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
                         }
-                    });
-                    if (!resp.ok) {
-                        throw new Error(`error status: ${resp.status}`);
+                        let data = await resp.json();
+                        setStore({ peopleInfo: { ...store.peopleInfo, [id]: data.result.properties } });
+                        localStorage.setItem("peopleInfo", JSON.stringify({ ...store.peopleInfo, [id]: data.result.properties }));
+                    } catch (error) {
+                        console.error("Error");
                     }
-                    let data = await resp.json();
-                    setStore({ peopleInfo: { ...getStore().peopleInfo, [id]: data.result.properties } });
-                } catch (error) {
-                    console.error("Error");
                 }
             },
             getPlanets: async () => {
-                try {
-                    const resp = await fetch("https://www.swapi.tech/api/planets?page=2&limit=6", {
-                        method: "GET",
-                        headers: {
-                            "Content-type": "application/json"
+                const store = getStore();
+                if (store.planets.length === 0) {
+                    try {
+                        const resp = await fetch("https://www.swapi.tech/api/planets?page=2&limit=6", {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
                         }
-                    });
-                    if (!resp.ok) {
-                        throw new Error(`error status: ${resp.status}`);
+                        let data = await resp.json();
+                        setStore({ planets: data.results });
+                        localStorage.setItem("planets", JSON.stringify(data.results));
+                    } catch (error) {
+                        console.error("Error");
                     }
-                    let data = await resp.json();
-                    setStore({ planets: data.results });
-                } catch (error) {
-                    console.error("Error");
                 }
             },
             getPlanetsInfo: async (id) => {
-                try {
-                    const resp = await fetch(`https://www.swapi.tech/api/planets/${id}`, {
-                        method: "GET",
-                        headers: {
-                            "Content-type": "application/json"
+                const store = getStore();
+                if (!store.planetsInfo[id]) {
+                    try {
+                        const resp = await fetch(`https://www.swapi.tech/api/planets/${id}`, {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
                         }
-                    });
-                    if (!resp.ok) {
-                        throw new Error(`error status: ${resp.status}`);
+                        let data = await resp.json();
+                        setStore({ planetsInfo: { ...store.planetsInfo, [id]: data.result.properties } });
+                        localStorage.setItem("planetsInfo", JSON.stringify({ ...store.planetsInfo, [id]: data.result.properties }));
+                    } catch (error) {
+                        console.error("Error");
                     }
-                    let data = await resp.json();
-                    setStore({ planetsInfo: { ...getStore().planetsInfo, [id]: data.result.properties } });
-                } catch (error) {
-                    console.error("Error");
                 }
             },
             getVehicles: async () => {
-                try {
-                    const resp = await fetch("https://www.swapi.tech/api/vehicles?page=1&limit=10", {
-                        method: "GET",
-                        headers: {
-                            "Content-type": "application/json"
+                const store = getStore();
+                if (store.vehicles.length === 0) {
+                    try {
+                        const resp = await fetch("https://www.swapi.tech/api/vehicles?page=1&limit=60", {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
                         }
-                    });
-                    if (!resp.ok) {
-                        throw new Error(`error status: ${resp.status}`);
+                        let data = await resp.json();
+                        setStore({ vehicles: data.results });
+                        localStorage.setItem("vehicles", JSON.stringify(data.results));
+                    } catch (error) {
+                        console.error("Error");
                     }
-                    let data = await resp.json();
-                    setStore({ vehicles: data.results });
-                } catch (error) {
-                    console.error("Error");
                 }
             },
             getVehiclesInfo: async (id) => {
-                try {
-                    const resp = await fetch(`https://www.swapi.tech/api/vehicles/${id}`, {
-                        method: "GET",
-                        headers: {
-                            "Content-type": "application/json"
+                const store = getStore();
+                if (!store.vehiclesInfo[id]) {
+                    try {
+                        const resp = await fetch(`https://www.swapi.tech/api/vehicles/${id}`, {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
                         }
-                    });
-                    if (!resp.ok) {
-                        throw new Error(`error status: ${resp.status}`);
+                        let data = await resp.json();
+                        setStore({ vehiclesInfo: { ...store.vehiclesInfo, [id]: data.result.properties } });
+                        localStorage.setItem("vehiclesInfo", JSON.stringify({ ...store.vehiclesInfo, [id]: data.result.properties }));
+                    } catch (error) {
+                        console.error("Error");
                     }
-                    let data = await resp.json();
-                    setStore({ vehiclesInfo: { ...getStore().vehiclesInfo, [id]: data.result.properties } });
-                } catch (error) {
-                    console.error("Error");
                 }
             },
             addFavorite: (type, id, name) => {
                 const store = getStore();
                 const url = `/details/${type}/${id}`;
                 if (!store.favorites.find(fav => fav.name === name)) {
-                    setStore({ favorites: [...store.favorites, { type, id, name, url }] });
+                    const newFavorites = [...store.favorites, { type, id, name, url }];
+                    setStore({ favorites: newFavorites });
+                    localStorage.setItem("favorites", JSON.stringify(newFavorites));
                 }
             },
             removeFavorite: (name) => {
                 const store = getStore();
-                setStore({ favorites: store.favorites.filter(fav => fav.name !== name) });
+                const newFavorites = store.favorites.filter(fav => fav.name !== name);
+                setStore({ favorites: newFavorites });
+                localStorage.setItem("favorites", JSON.stringify(newFavorites));
             }
         }
     };
